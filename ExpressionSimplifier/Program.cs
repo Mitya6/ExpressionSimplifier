72681ec9411d4258b0590ad17db15e01c934e99c
Mitya6 <mitya6@hotmail.com>
2014-03-23 15:13:49 +0100

    Expression tree building and dimension checking.

--- ExpressionSimplifier/Program.cs
+++ ExpressionSimplifier/Program.cs
@@ -8,8 +8,33 @@ namespace ExpressionSimplifier
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
+            String path = "Input.txt";
+            List<String> lines = LineByLineReader.ReadInput(path);
+
+            List<ExpressionTree> trees = new List<ExpressionTree>();
+            foreach (String line in lines)
+            {
+                trees.Add(new ExpressionTree(line));
+            }
+
+
+            foreach (var item in trees)
+            {
+                Dimension dim;
+                try
+                {
+                    dim = item.GetDimensions();
+                }
+                catch (ApplicationException appEx)
+                {
+                    Console.WriteLine(appEx.Message);
+                    continue;
+                }
+                Console.WriteLine("\n\n [" + dim.N + "," + dim.M + "]");
+                Console.WriteLine(item.ToString());
+            }
         }
     }
 }
